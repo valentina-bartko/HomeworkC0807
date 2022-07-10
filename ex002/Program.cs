2ccb8641ad8fe55464519729e0d692269a0a51f4
VBartko <drama.queen@internet.ru>
2022-07-10 18:19:32 +0300

    Changed the second block diagram

--- ex002/Program.cs
+++ ex002/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 2. Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
+// Я понимаю задачу так:
+// написать программу, которая принимает на вход три целых числа, введенных пользователем
+// и выдает максимальное из этих чисел.
+
+Console.Write("Введите три целых числа: ");
+string x, y, z = Console.ReadLine();
+
+int[] numbers = {x, y, z};
+int index = 0;
+int max = numbers[0];
+
+while(index < numbers.Length)
+{
+    if(numbers[index] > max)
+    {
+        max = numbers[index];
+    }
+    index++;
+}
+Console.WriteLine(max);
